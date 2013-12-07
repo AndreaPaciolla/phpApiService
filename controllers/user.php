39c91dc049f5d42d83f657ac67db724b531798c9
andrea paciolla <andreapaciolla@gmail.com>
2013-12-07 16:22:36 +0100

    added htaccess file - errors exception to controllers

--- controllers/user.php
+++ controllers/user.php
@@ -2,17 +2,23 @@
 
 
 
-class UserController {
+class UserController {	
 
 	public static function getUser($id) {
-	    $user = User::find_by_id($id)->to_array();
 
-	    echo json_encode( $user );
+		if ( $user = User::find_by_id( (int)$id ) )
+			echo json_encode( $user->to_array() );
+		else
+			echo json_encode( array("error" => true) );
+		
 	}
 
 	public static function getUsers() {
-		$users = User::all()->to_array();
-	    echo json_encode( $users );
+		#die( var_dump( User::all() ) );
+		if( $users = User::all() )
+			echo json_encode( $users->to_array() );
+		else
+			echo json_encode( array("error" => true) );	
 	}
 
 
